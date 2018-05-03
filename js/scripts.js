da255d03306e02eefe20e8cedd5259830495cf8f
first-name second-name <iamfeysal@gmail.com>
2018-05-03 11:23:39 +0300

    added additional specs

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,12 @@ $(document).ready(function() {
     };
 
     // jQuery user interface logic here.
+    var leapYear = function(year) {
+   if ((year % 4 === 0) && (year % 100 !== 0)) {
+     return true;
+   } else {
+     return false;
+   }
+ };
+
+ // jQuery user interface logic here.
